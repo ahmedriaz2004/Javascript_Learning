ed247c3fa048926dbc6be2307b5fb1e3d0ee620b
ahsenrz <ahsenriaz2001@gmail.com>
2022-09-18 01:13:58 +0500

    javascript learning

---
+++ file1.js
@@ -0,0 +1,121 @@
+
+// // // console.log can print anything in console//
+// // console.log("hello world");
+
+
+// var firstName = "ahmed";
+// console.log("firstName",firstName);
+
+// var value1 = 2;
+// console.log(value1 **2);
+
+
+// var myAge = 91;
+// console.log(myAge);
+
+
+
+            //    SPECIALSYMBOLS   //
+
+
+
+// var $firstname = "ahmed riaz";
+// console.log($firstname);
+
+// // var myArea = "a33-gulshan";
+// // console.log(myArea);
+
+
+                // CONSTANTS//
+
+
+// const pi = 3.14;
+// console.log(pi);
+
+// var firtCharacter ="waseem";
+// console.log(firtCharacter[5]);
+
+// var firtCharacter = "waseeemriazwerwerwerwerwer";
+// console.log(firtCharacter[firtCharacter.length-1]);
+
+
+// var myName = "  ahmedriaz  "
+// console.log(myName.length);
+// var newString = myName.trim();
+// console.log(newString.length);
+
+            //    UPPERCASE   //
+           //     LOWERCASE   //
+          //      TRIM        //
+         //       SLICE       //
+
+
+// var myName = "muhammedahmedriaz";
+// console.log(myName.toUpperCase());
+
+// var myName ="ahmedriaz";
+// console.log(myName.slice(2,7));
+
+
+
+                       // TYPEOF //
+                    //    NUMBER TO STRING  // 
+                    //    STRING TO NUMBER  //  
+
+// var age = 22;
+// var name = "ahmed";
+// console.log(typeof(age + ""));
+
+
+// var myString = +"999";
+// console.log( typeof myString);
+
+
+// var age = 19;
+// age = String(age);
+// console.log(typeof age);
+
+
+// var age = 19;
+// age = Number(age);
+// console.log(typeof age);
+
+            // ADDING MORE THAN ONE STRING //
+
+// var String1 = "muhammed";
+// var String2 = "ahmed";
+// var String3 = "riaz";
+// var fullname = String1 +" "+ String2 +" "+ String3;
+// console.log(fullname);
+
+
+            // ADDING MORE THAN ONE NUMBER //
+
+
+// var Number1 = 10;
+// var Number2 = 10;
+// var Number3 = 10;
+// var fullNumber = Number1 + Number2 + Number3;
+// console.log(fullNumber);
+
+
+                //  TEMPLATE STRING  //
+
+
+// var age = 29;
+// var Name = "ahmed riaz";
+// var aboutMe = `my name is ${Name} and my age is ${age}`;
+// console.log(aboutMe);
+
+
+
+
+
+
+
+
+
+
+
+
+
