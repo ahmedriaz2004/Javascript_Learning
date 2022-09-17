ecde8b861421bd68cc2f21e3ed113e9800dacaa3
ahsenrz <ahsenriaz2001@gmail.com>
2022-09-18 01:32:26 +0500

    javascript learning

--- file1.js
+++ file1.js
@@ -1,7 +1,5 @@
-
-// // // console.log can print anything in console//
-// // console.log("hello world");
-
+//  console.log can print anything in console//
+//  console.log("hello world");
 
 // var firstName = "ahmed";
 // console.log("firstName",firstName);
@@ -9,25 +7,18 @@
 // var value1 = 2;
 // console.log(value1 **2);
 
-
 // var myAge = 91;
 // console.log(myAge);
 
+//    SPECIALSYMBOLS   //
 
+// var $firstName = "ahmed riaz";
+// console.log($firstName);
 
-            //    SPECIALSYMBOLS   //
+//  var myArea = "a33-gulshan";
+//  console.log(myArea);
 
-
-
-// var $firstname = "ahmed riaz";
-// console.log($firstname);
-
-// // var myArea = "a33-gulshan";
-// // console.log(myArea);
-
-
-                // CONSTANTS//
-
+// CONSTANTS//
 
 // const pi = 3.14;
 // console.log(pi);
@@ -38,17 +29,15 @@
 // var firtCharacter = "waseeemriazwerwerwerwerwer";
 // console.log(firtCharacter[firtCharacter.length-1]);
 
-
 // var myName = "  ahmedriaz  "
 // console.log(myName.length);
 // var newString = myName.trim();
 // console.log(newString.length);
 
-            //    UPPERCASE   //
-           //     LOWERCASE   //
-          //      TRIM        //
-         //       SLICE       //
-
+//    UPPERCASE   //
+//     LOWERCASE   //
+//      TRIM        //
+//       SLICE       //
 
 // var myName = "muhammedahmedriaz";
 // console.log(myName.toUpperCase());
@@ -56,31 +45,26 @@
 // var myName ="ahmedriaz";
 // console.log(myName.slice(2,7));
 
-
-
-                       // TYPEOF //
-                    //    NUMBER TO STRING  // 
-                    //    STRING TO NUMBER  //  
+// TYPEOF //
+//    NUMBER TO STRING  //
+//    STRING TO NUMBER  //
 
 // var age = 22;
 // var name = "ahmed";
 // console.log(typeof(age + ""));
 
-
 // var myString = +"999";
 // console.log( typeof myString);
-
 
 // var age = 19;
 // age = String(age);
 // console.log(typeof age);
 
-
 // var age = 19;
 // age = Number(age);
 // console.log(typeof age);
 
-            // ADDING MORE THAN ONE STRING //
+// ADDING MORE THAN ONE STRING //
 
 // var String1 = "muhammed";
 // var String2 = "ahmed";
@@ -88,34 +72,28 @@
 // var fullname = String1 +" "+ String2 +" "+ String3;
 // console.log(fullname);
 
+// ADDING MORE THAN ONE NUMBER //
 
-            // ADDING MORE THAN ONE NUMBER //
-
-
-// var Number1 = 10;
+// var number1 = 10;
 // var Number2 = 10;
 // var Number3 = 10;
 // var fullNumber = Number1 + Number2 + Number3;
 // console.log(fullNumber);
 
-
-                //  TEMPLATE STRING  //
-
+//  TEMPLATE STRING  //
 
 // var age = 29;
-// var Name = "ahmed riaz";
+// var completed = true;
+// var name = "ahmed riaz";
 // var aboutMe = `my name is ${Name} and my age is ${age}`;
 // console.log(aboutMe);
 
+// LET Example convert number into string
+// let age = 10;
+// age = age.toString();
+// console.log("age", age)
 
-
-
-
-
-
-
-
-
-
-
-
+// CONST Example 
+// const age = 10;
+// age = age.toString()
+// console.log(age)
